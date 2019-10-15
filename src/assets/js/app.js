f3cb4f9aa4c5de813488417e386129ee94461a27
vladislav t <vt118@mail.ru>
2019-10-16 00:20:11 +0300

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,19 @@
+//= ../../../node_modules/slick-carousel/slick/slick.js
+
+// $(function(){
+//
+//     $('.header__slider').slick({
+//         infinite: true,
+//         fade: true,
+//         asNavFor: '.slider-dots',
+//         prevArrow: '<img class="slider-arrows slider-arrows__left" src="assets/img/arrow-left.svg">',
+//         nextArrow: '<img class="slider-arrows slider-arrows__right" src="assets/img/arrow-right.svg">'
+//     });
+//
+//     $('.slider-dots').slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 4,
+//         asNavFor: '.header__slider'
+//
+//     });
+// });
